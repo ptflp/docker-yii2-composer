5d054b58b98f88155b4cbe5ef4849049a2015f16
ptflp <filippov.pa@s-vfu.ru>
2018-07-23 21:51:48 +0900

    add project init sh

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -4,5 +4,16 @@ set -e
 rm -f /run/apache2.pid
 rm -f /run/apache2/apache2.pid
 rm -f /var/run/apache2/apache2.pid
-ulimit -s unlimited
+file="/var/www/project-init.bash"
+if [ -f "$file" ]
+then
+	if [[ $INIT != 1 ]]; then
+		sleep 21
+		cd /var/www/
+		bash /var/www/project-init.bash
+	fi
+else
+	echo "$file not found."
+fi
+export INIT=1
 exec apache2 -DFOREGROUND
